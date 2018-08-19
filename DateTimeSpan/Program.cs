fa67f27f2283e77e1a71bf0c75e357b6d2821c5a
myasinK <yasinul@gmail.com>
2018-08-18 19:15:41 -0700

    Finish drill on datetime obj

---
+++ DateTimeSpan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number: ");
+            double num = Convert.ToInt32(Console.ReadLine());
+            DateTime then = now.AddHours(num);
+            Console.WriteLine("time listed in line 1 plus hours you added equates to a datime obj of:\n");
+            Console.WriteLine(then);
+            
+        }
+    }
+}
